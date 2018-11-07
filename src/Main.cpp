dd19c9938f9bf5447a5694cd9b5fd03597e15780
McMurphy Luo <McMurphy.Luo@gmail.com>
2018-11-07 22:56:16 +0800

    save my work, try to write a optional template class my own

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,35 +1,35 @@
 #include <type_traits>
 #include <iostream>
 #include <cmath>
+#include <iostream>
+#include "optional.h"
 
 using namespace std;
 
-template<typename T>
-class A {
+class TT {
 public:
-  A() {
+  TT(int k) : k_(k) {
 
   }
+  
+  TT(const TT&) = delete;
 
-  
-  template<
-    typename std::enable_if<std::is_copy_assignable<T>::value && std::is_copy_constructible<T>::value, int>::type = 0
-  >
-  
-  A(const A& another) {
-    cout << "fucker" << endl;
-  }
+  int k_;
 };
 
 int main(int argc, char* argv) {
-  cout
-    << "double_t copy assignable: " << std::is_copy_assignable<double_t>::value << endl
-    << "double_t copy constructable" << std::is_copy_constructible<double_t>::value << endl;
+  optional<double> db;
+  db = 5.4;
 
-  A<double_t> nihao;
-  A<double_t> another = nihao;
+  
 
-  cout << "good" << endl;
+  cout << typeid(db).name() << endl;
+
+  cout << typeid(optional<TT>).name() << endl;
+
+  cout << db.value() << std::endl;
+
+  
 
   getchar();
 }
---
+++ src/optional.h
@@ -0,0 +1,134 @@
+#ifndef LEARNING_OPTIONAL_H_
+#define LEARNING_OPTIONAL_H_
+
+#include <type_traits>
+#include <memory>
+#include <cassert>
+
+template<typename T>
+struct optional_base
+{
+  optional_base()
+    : value_(nullptr)
+  {
+
+  }
+
+  optional_base(nullptr_t)
+    : value_(nullptr)
+  {
+
+  }
+
+  optional_base(optional_base&& another)
+    : value_(std::move(another.value_))
+  {
+
+  }
+
+  optional_base& operator=(optional_base<T>&& another)
+  {
+    value_ = std::move(another.value_);
+    return *this;
+  }
+
+  operator bool() const {
+    return has_value();
+  }
+
+  bool has_value() const {
+    if (value_) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  T& value() {
+    assert(value_);
+    return *value_;
+  }
+
+  const T& value() const {
+    assert(value_);
+    return *value_;
+  }
+
+protected:
+  std::unique_ptr<T> value_;
+};
+
+template<typename T>
+class optional_copyable : public optional_base<T>
+{
+public:
+  optional_copyable()
+    : optional_base<T>()
+  {
+
+  }
+
+  optional_copyable(nullptr_t)
+    : optional_base<T>(nullptr)
+  {
+
+  }
+
+  optional_copyable(const optional_copyable& another)
+    : optional_base<T>(nullptr)
+  {
+    if (another.has_value()) {
+      this->value_ = std::make_unique<T>(another.value());
+    }
+  }
+  
+  optional_copyable& operator=(const optional_copyable& another)
+  {
+    if (this == &another) {
+      return *this;
+    }
+    if (!another.has_value()) {
+      this->value_.reset();
+    }
+    else if (another.has_value() && this.has_value()) {
+      *(this.value_) = *(another.value_);
+    }
+    else {
+      this.value_ = std::make_unique<T>(*(another.value_));
+    }
+    return *this;
+  }
+  
+  optional_copyable(const T& value)
+    : optional_base(nullptr)
+  {
+    if (this.has_value()) {
+      *(this.value_) = value;
+    }
+    else {
+      this.value_ = std::make_unique<T>(value);
+    }
+  }
+
+  optional_copyable& operator=(const T& another)
+  {
+    if (this->has_value()) {
+      *(this->value_) = another;
+    }
+    else {
+      this->value_ = std::make_unique<T>(another);
+    }
+    return *this;
+  }
+};
+
+template<typename T>
+using optional =
+typename std::conditional<
+  std::is_copy_assignable<T>::value && std::is_copy_constructible<T>::value,
+  optional_copyable<T>,
+  optional_base<T>
+>::type;
+
+#endif // LEARNING_OPTIONAL_H_
